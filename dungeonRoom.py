e802b53acd42197537a8117fd9f0611bca5f47d8
chauler <amt1309@gmail.com>
2022-03-25 14:32:02 -0500

    Added slider to adjust number of rooms, added button to generate image dynamically, added image cropping to allow for bigger tiles with larger room numbers.

--- dungeonRoom.py
+++ dungeonRoom.py
@@ -1,6 +1,7 @@
+from email.mime import image
 from operator import truediv
 import random
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
 
 class Room:
     def __init__(self, size=5):
@@ -18,9 +19,13 @@ def CheckCollision(room, rooms, map): #true if collision, false if valid
     if room.x + room.length >= map.size or room.y + room.height >= map.size: #checking with map bounds
         return True
     count=0
-    for y in range(room.y, room.y+room.height):
-        for x in range(room.x, room.x+room.length):
+    for y in range(room.y, room.y+room.height+1):
+        for x in range(room.x, room.x+room.length+1):
+    #        print('x=',x,' y=', y)
             count = count+1 if map.grid[y][x].status > 1 else count
+    #        print(count)
+    if count < 3 and len(rooms) != 0:
+        return True
     for obj in rooms: #passed bounds check, checking with other rooms
         if room is obj:
             continue
@@ -37,23 +42,44 @@ class Map:
         self.size = size
         self.grid = [[Square() for x in range(size)] for y in range(size)]
 
-def DrawPicture(map:Map):
-    image = Image.new("RGB", (10*map.size, 10*map.size))
+def TrimImage(dimensions, map):
+    dimensions[0]=map.size
+    dimensions[1]=0
+    dimensions[2]=map.size
+    dimensions[3]=0
+    for indexy, y in enumerate(map.grid): #grabs farthest left wall
+        for indexx, x in enumerate(y):
+            if x.status > 1: #if cell is wall
+                dimensions[0] = indexy if indexy < dimensions[0] else dimensions[0]
+                dimensions[1] = indexy if indexy > dimensions[1] else dimensions[1]
+                dimensions[2] = indexx if indexx < dimensions[2] else dimensions[2]
+                dimensions[3] = indexx if indexx > dimensions[3] else dimensions[3]
+
+def DrawPicture(map:Map, ppi):
+    dimensions = [map.size, 0, map.size, 0] #0=miny 1=maxy 2=minx 3=maxx
+    TrimImage(dimensions, map)
+    #print(map.size)
+    #print(dimensions)
+    image = Image.new("RGB", ((dimensions[3]-dimensions[2])*ppi+ppi, (dimensions[1]-dimensions[0])*ppi+ppi))
     draw = ImageDraw.Draw(image)
     #for each cell, draw 10x10 pixels
-    for indexy, y in enumerate(map.grid):
-        for indexx, x in enumerate(y):
-            if x.status == 1:
-                draw.rectangle([indexx*10, indexy*10, (indexx*10)+10, (indexy*10)+10], outline="black", fill = "brown")
-            elif x.status > 1:
-                draw.rectangle([indexx*10, indexy*10, (indexx*10)+10, (indexy*10)+10], outline="black", fill = "grey")
-    image.show()
-    image = image.resize((500,500), Image.ANTIALIAS)
+    for y in range(dimensions[0], dimensions[1]+1):
+        for x in range(dimensions[2], dimensions[3]+1):
+            if map.grid[y][x].status == 1:
+               # print([(x-dimensions[2])*ppi,(y-dimensions[0])*ppi,((x-dimensions[2])*ppi)+ppi,((y-dimensions[0])*ppi)+ppi])
+                #print('floor: ',y,' ', x)
+                draw.rectangle([(x-dimensions[2])*ppi,(y-dimensions[0])*ppi,((x-dimensions[2])*ppi)+ppi,((y-dimensions[0])*ppi)+ppi], outline="black", fill = "brown")
+            elif map.grid[y][x].status > 1:
+                #print('wall: ', y,' ', x)
+                draw.rectangle([(x-dimensions[2])*ppi,(y-dimensions[0])*ppi,((x-dimensions[2])*ppi)+ppi,((y-dimensions[0])*ppi)+ppi], outline="black", fill = "grey")
+                #print([(x-dimensions[2])*ppi,(y-dimensions[0])*ppi,((x-dimensions[2])*ppi)+ppi,((y-dimensions[0])*ppi)+ppi])
+    #image.show()
+    image = image.resize((750,750), Image.ANTIALIAS)
     return image
 
 def GenRooms(rooms, numRooms):
     for x in range(numRooms): #generate list of rooms with random sizes
-        rooms.append(Room(12))
+        rooms.append(Room(10))
     
 def GetMaxDimension(rooms):
     totalLength=0
@@ -77,16 +103,51 @@ def PlaceRooms(rooms, placedRooms, map):
             else: #breaks only if move is valid and doesnt go oob
                 break
     for room in rooms:
-        room.UpdateVertices(coord)
+        room.UpdateVertices(coord) #place the room on a hypothetical position, then adjust position in while loop
         while CheckCollision(room, placedRooms, map): #while there is a collision with border or existing rooms
             move = random.choice(dir)
             coord[0] += move[0]
             coord[1] += move[1]
-            room.UpdateVertices(coord)
+            if coord[0] >= map.size or coord[1] >= map.size or coord[0] < 0 or coord[1] < 0: #go oob
+                coord[0] -= move[0] #undo move and try again
+                coord[1] -= move[1]
+            room.UpdateVertices(coord) #update with new coords, try again
+
         placedRooms.append(room)
         for x in range(room.x, room.x+room.length+1):
             for y in range(room.y, room.y+room.height+1):
                     if x == room.x or y == room.y or x == room.x+room.length or y == room.y+room.height:
                         map.grid[y][x].status += 2 #make walls 2
                     else:
-                        map.grid[y][x].status += 1 #make cells occupied
+                        map.grid[y][x].status = 1 #make cells occupied
+
+def GenerateMap(numRooms, ppi):
+    random.seed()
+    rooms = [] #initial roomlist
+    placedRooms = [] #rooms already placed in map, this is used for collision checking
+
+    GenRooms(rooms, numRooms)
+
+    maxDimension = GetMaxDimension(rooms)
+
+    map = Map(5+maxDimension) #generates map big enough for the rooms + a buffer
+
+#place rooms on the map, random walk from center
+    PlaceRooms(rooms, placedRooms, map)
+    #for y in map.grid:
+    #    for x in y:
+    #        print(x.status, end='')
+    #    print('')
+    #for room in placedRooms:
+    #    print(room.x, ' ', room.y, ' ', room.x+room.length, ' ', room.y+room.height)
+    image = DrawPicture(map, ppi)
+    image = ImageTk.PhotoImage(image)
+    return image
+
+def ButtonCallback(numRooms, imgLabel, imgFrame, ppi):
+    image = GenerateMap(numRooms, ppi)
+    imgLabel.configure(image=image)
+    imgLabel.image = image
+
+def ScaleCallback(numRooms, label):
+    pass
--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 from ctypes import resize
 import dungeonRoom as dr
+import math
 import random
 import tkinter as tk
 from tkinter import ttk
@@ -7,30 +8,23 @@ from PIL import ImageTk, Image
 window = tk.Tk()
 #window.geometry('1280x720')
 window.resizable(False, False)
+image = Image.new("RGB", (10, 10))
 imageFrame = ttk.Frame(window)
+imgLabel = ttk.Label(imageFrame)
 UIFrame = ttk.Frame(window)
-numRooms = tk.DoubleVar()
-roomNumScale = ttk.Scale(UIFrame, from_=1, to=100, variable=numRooms)
-
-random.seed()
-rooms = [] #initial roomlist
-placedRooms = [] #rooms already placed in map, this is used for collision checking
-
-dr.GenRooms(rooms, numRooms)
-
-maxDimension = dr.GetMaxDimension(rooms)
-
-map = dr.Map(5+maxDimension) #generates map big enough for the rooms + a buffer
-
-#place rooms on the map, random walk from center
-dr.PlaceRooms(rooms, placedRooms, map)
-
-image = dr.DrawPicture(map)
-image = ImageTk.PhotoImage(image)
-imgLabel = ttk.Label(imageFrame, image=image)
+numRooms = 10
+ppi=40
+scaleLabel = ttk.Label(UIFrame, text='# of Rooms: ')
+valueLabel = ttk.Label(UIFrame, text="10")
+roomNumScale = ttk.Scale(UIFrame, from_=1, to=50, value=10, command= lambda event: valueLabel.configure(text='{:.0f}'.format(math.floor(roomNumScale.get()))))
+genButton = ttk.Button(UIFrame, text='Generate', command= lambda: dr.ButtonCallback(math.floor(roomNumScale.get()), imgLabel, imageFrame, ppi))
+roomNumScale.grid(row=2, column=0, sticky='n')
+valueLabel.grid(row=1,column=0)
+scaleLabel.grid(row=0, column=0)
+genButton.grid(row=3, column=0)
+image = dr.GenerateMap(numRooms, ppi)
+imgLabel.configure(image=image)
 imgLabel.grid(row=0, column=0)
-imgLabel = ttk.Label(UIFrame, image=image)
-imgLabel.grid(row=0,column=0)
 imageFrame.grid(row=0,column=0)
 UIFrame.grid(row=0,column=1)
 window.mainloop()
